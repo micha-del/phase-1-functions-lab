144e554ff55d424388ef628323f6cd342daaa2f8
Michael Kiros <michael.kiros00@gmail.com>
2022-11-28 19:49:15 -0500

    commit tree a5908be5dff0acfadacaea9f83df82a1c05ab9c7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42) 
+}
+function distanceFromHqInFeet(someValue) {
+    return Math.abs((someValue - 42) * 264 )
+}
+function distanceTravelledInFeet(value1, value2) {
+    return Math.abs((value1 - value2) * 264)
+}
+function calculatesFarePrice(start, destination) {
+    const a = distanceTravelledInFeet(start, destination)
+    if (400 > a) {
+        return 0
+    }
+    else if (400 < a && a < 2000) {
+        return (a-400) * 0.02
+    }
+    else if (2000 < a && a < 2500) {
+        return 25
+    }
+    else if (a > 2500) {
+        return "cannot travel that far"
+    }
+}
